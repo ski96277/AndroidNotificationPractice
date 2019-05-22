db5ffe2665f813042280c4053b7f419d22c2596a
ski96277 <imran.bu96.com>
2019-05-22 11:15:24 +0600

    Added Service for alert view

--- app/src/main/java/com/example/androidnotificationpractice/MyService.java
+++ app/src/main/java/com/example/androidnotificationpractice/MyService.java
@@ -1,34 +1,45 @@
 package com.example.androidnotificationpractice;
 
+
 import android.app.Service;
+
 import android.content.ComponentName;
+
 import android.content.Intent;
+
 import android.os.IBinder;
+
 import android.support.annotation.NonNull;
+
 import android.util.Log;
+
 import android.widget.Toast;
 
+
 import com.google.firebase.database.DataSnapshot;
+
 import com.google.firebase.database.DatabaseError;
+
 import com.google.firebase.database.DatabaseReference;
+
 import com.google.firebase.database.FirebaseDatabase;
+
 import com.google.firebase.database.ValueEventListener;
+
 
 public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // Let it continue running until it is stopped.
-
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
-
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
                 String id = dataSnapshot.child("Call").getValue().toString();
                 if (id.equals("1")) {
+
                     startActivity(new Intent(getApplicationContext(), AlertActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
                 }
             }
@@ -37,9 +48,8 @@ public class MyService extends Service {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
         });
-
-
         return START_STICKY;
     }
 
@@ -47,5 +57,4 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
-
 }
